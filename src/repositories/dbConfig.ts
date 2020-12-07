2efff2c54799dd2584a15eb107cdac952299f058
simon.mor <simon.mor@incredibuild.com>
2020-12-07 17:18:47 +0200

    db config added

--- src/repositories/dbConfig.ts
+++ src/repositories/dbConfig.ts
@@ -1,9 +1,25 @@
-const postgresConnection = {
+const postgresConnectionLocal = {
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
     password: 'pgadmin4320',
     port: 5432,
 }
+
+const postgresConnection = {
+    user: 'wmcqthqf',
+    host: 'postgres://wmcqthqf:zXT41Mneqso7kQjoULlxG-n8nMMK1XOk@kandula.db.elephantsql.com:5432',
+    database: 'wmcqthqf',
+    password: 'zXT41Mneqso7kQjoULlxG-n8nMMK1XOk',
+    port: 5432,
+}
+
+// const postgresConnection = {
+//     user: 'root',
+//     host: 'postgresql://SG-food-1483-pgsql-master.servers.mongodirector.com',
+//     database: 'postgres',
+//     password: 'Qpalzm12!@',
+//     port: 5432,
+// }
 
 export default postgresConnection;
